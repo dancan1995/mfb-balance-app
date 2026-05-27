aaa2628fec4de74abe016da42c61a9e9e9eead1b
dancan1995 <dancanodhiambo95@gmail.com>
2026-05-26 21:41:17 -0400

    Initial commit - MFB Balance App

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -23,15 +23,14 @@ import {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
-// CORRECT Firebase Configuration from Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyALhhh-dgrsAHq15a539H6hMfRJIx9fiPo",
-  authDomain: "mfbcat-b5fbe.firebaseapp.com",
-  projectId: "mfbcat-b5fbe",
-  storageBucket: "mfbcat-b5fbe.firebasestorage.app",
-  messagingSenderId: "891402928607",
-  appId: "1:891402928607:web:81e52c72d99906eca1c46a",
-  measurementId: "G-9C426CGWHV"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Singleton instance
